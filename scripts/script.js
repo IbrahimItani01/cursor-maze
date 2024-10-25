bba264369d349b83a8e34b6927339021e25eda48
Ibrahim Itani <I.itani2002@gmail.com>
2024-10-26 01:03:19 +0300

    created a win h3 tag

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,9 @@
 const start = document.getElementById("start");
 const end = document.getElementById("end");
 const boundaries = document.querySelectorAll(".boundary");
+const body = document.querySelector("body")
+var winMsg = document.createElement("h3");
+
 start.addEventListener("mouseleave", function () {
   for (i in boundaries) {
     boundaries[i].addEventListener("mouseenter", function () {
@@ -8,8 +11,13 @@ start.addEventListener("mouseleave", function () {
       window.location.reload();
     });
     end.addEventListener("mouseleave", function () {
-      window.location.reload();
-      alert("Congratulations, you won!");
+        winMsg.innerText="Congratulations, you won!";
+        body.appendChild(winMsg);
+        setTimeout(
+            function(){
+                window.location.reload()
+            }
+            ,3000);
     });
   }
 });
